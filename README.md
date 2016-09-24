79fb0bce0b4938bae57c23d56f5f7df6df333af1
Aman Gebru <awolde@amanggb.me>
2016-09-24 14:58:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+checkPower.sh
+====================
+Check if your server is up or not and send email
+
+Usage
+=======
+Better to run in screen or tmux session in an infinite loop
+```while true; do ./checkPower.sh; done```
---
+++ checkPower.sh
@@ -0,0 +1,28 @@
+REMOTE="your.remote.server"
+PORT="22"
+EMAIL="your@email.com"
+emailSent=0
+powerLoss=0
+
+while true
+do
+  echo "I'm checking for power .... `date`"
+  nc -z -w 1 $REMOTE $PORT
+  if [ $? -ne 0 ]; then
+    powerLoss=$(($powerLoss + 1))
+    if [[ $emailSent -lt 3 && $powerLoss -gt 5 ]]; then
+      echo "Sending email to $EMAIL"
+      echo "Power outage detected. `date`" | mail -s "ALERT: Power loss" -a "From: alert@fromblah.com" $EMAIL
+      emailSent=$(($emailSent + 1))
+    fi
+
+  else
+    if [ $emailSent -gt 2 ]; then
+      echo "Power is restored. `date`" | mail -s "ALERT: Power restored" -a "From: alert@fromblah.com" $EMAIL
+      powerLoss=0
+      emailSent=0
+      continue
+    fi
+  fi
+  sleep 30
+done
